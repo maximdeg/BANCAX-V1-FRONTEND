db99fbd71671d5c10af7c91fe0f4f24457f2fb86
Maxim Degtiarev <mxim.degt@gmail.com>
2024-11-29 10:42:44 -0300

    changes

--- src/Components/ForgotPasswordForm/ForgotPasswordForm.jsx
+++ src/Components/ForgotPasswordForm/ForgotPasswordForm.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 import "./ForgotPasswordForm.css";
 import { Link } from "react-router-dom";
+import ENV from "../../env.js";
+import { getUnnauthenticatedHeaders } from "../../utils/Headers";
+import { POST } from "../../fetching/http.fetching";
 
 const ForgotPasswordForm = () => {
+    const handleSubmitEmail = async (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        console.log(email);
+
+        const response = await POST(`${ENV.API_URL}/api/v1/auth/forgot-password`, {
+            headers: getUnnauthenticatedHeaders(),
+            body: JSON.stringify({ email }),
+        });
+
+        console.log(response);
+    };
     return (
         <div className="login-container">
             <h2> Password Reset Request</h2>
             {/* <h3>Welcome back!</h3> */}
-            <form action="" className="login-form">
+            <div>
+                <p>Enter your email and we will send you a link to your email to reset your password.</p>
+                <span>
+                    If you don't have an account please
+                    <Link className="link link-signup" to="/in/register">
+                        {" "}
+                        Sign Up
+                    </Link>
+                </span>
+            </div>
+            <form className="login-form" onSubmit={(e) => handleSubmitEmail(e)}>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" />
                 </div>
-
                 <div className="btn-container">
-                    <button className="btn btn-signin">Sign In</button>
+                    <button className="btn btn-signin">Send email</button>
                 </div>
             </form>
             <div className="link-container">
